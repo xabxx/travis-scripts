1e70cf4b2ffd2dd37f8580a16b088c15603534ad
Abby Xu <abbyxu@amazon.com>
2019-01-30 00:29:53 -0800

    test travis builds

--- ros2_build.sh
+++ ros2_build.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-OIFS=$IFS
-IFS=","
-
-ros2_distro_names_arr=($ros2_distro_names)
-
-for (( i=0; i<${#ros2_distro_names_arr[@]}; i++ ));
-do
-  docker pull ros:${ros2_distro_names_arr[$i]}-ros-core
-  docker run -v "$PWD/shared:/shared" -e ROS_DISTRO=${ros2_distro_names_arr[$i]} --name ${ros2_distro_names_arr[$i]}-container -dit ros:${ros2_distro_names_arr[$i]}-ros-core /bin/bash
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'mkdir -p /"$ROS_DISTRO"_ws/src'
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3 python3-pip libgtest-dev cmake && rosdep update'
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib'
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools'
-  docker cp $TRAVIS_BUILD_DIR ${ros2_distro_names_arr[$i]}-container:/${ros2_distro_names_arr[$i]}_ws/src/
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y'
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage"'
-  if [[ -z "${PACKAGE_NAME}" ]]; then
-    docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --packages-select $PACKAGE_NAME --cmake-target tests'
-  fi
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test'
-  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test-result --all'
-done
-
-IFS=$OIFS;
+docker pull ros:"$ROS_DISTRO"-ros-core
+docker run -v "$PWD/shared:/shared" -e ROS_DISTRO="$ROS_DISTRO" --name "$ROS_DISTRO"-container -dit ros:"$ROS_DISTRO"-ros-core /bin/bash
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'mkdir -p /"$ROS_DISTRO"_ws/src'
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'apt update && apt install -y python3 python3-pip libgtest-dev cmake && rosdep update'
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib'
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools'
+docker cp $TRAVIS_BUILD_DIR "$ROS_DISTRO"-container:/"$ROS_DISTRO"_ws/src/
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y'
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage"'
+if [[ -z "${PACKAGE_NAME}" ]]; then
+  docker exec "$ROS_DISTRO"-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --packages-select $PACKAGE_NAME --cmake-target tests'
+fi
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test'
+docker exec "$ROS_DISTRO"-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test-result --all'
