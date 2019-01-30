bd27943c403962d00b7cd15aef1b35c09b2c04fb
Abby Xu <abbyxu@amazon.com>
2019-01-29 22:14:45 -0800

    reduce redundency

--- ros2_build.sh
+++ ros2_build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# ROS2 Bouncy Build
-docker pull ros:bouncy-ros-core
-docker run --name bouncy-container -dit ros:bouncy-ros-core /bin/bash
-docker exec bouncy-container /bin/bash -c 'mkdir -p /ros2_ws/src'
-docker exec bouncy-container /bin/bash -c 'apt update && apt install -y python3 python3-pip python3-colcon-common-extensions lcov python-rosdep && rosdep update'
-docker exec bouncy-container /bin/bash -c 'pip3 install -U setuptools'
-docker cp $TRAVIS_BUILD_DIR bouncy-container:/ros2_ws/src/
-docker exec bouncy-container /bin/bash -c 'source /opt/ros/bouncy/setup.bash && cd /ros2_ws/ && rosdep install --from-paths src --ignore-src --rosdistro bouncy -r -y'
-docker exec bouncy-container /bin/bash -c 'source /opt/ros/bouncy/setup.bash && cd /ros2_ws/ && colcon build --symlink-install'
-docker exec bouncy-container /bin/bash -c 'source /opt/ros/bouncy/setup.bash && cd /ros2_ws/ && colcon test'
-docker exec bouncy-container /bin/bash -c 'source /opt/ros/bouncy/setup.bash && cd /ros2_ws/ && colcon test-result --all'
+OIFS=$IFS
+IFS=","
 
-# ROS2 Crystal Build
-docker pull ros:crystal-ros-core
-docker run --name crystal-container -dit ros:crystal-ros-core /bin/bash
-docker exec crystal-container /bin/bash -c 'mkdir -p /ros2_ws/src'
-docker exec crystal-container /bin/bash -c 'apt update && apt install -y python3 python3-pip python3-colcon-common-extensions lcov python-rosdep && rosdep update'
-docker exec crystal-container /bin/bash -c 'pip3 install -U setuptools'
-docker cp $TRAVIS_BUILD_DIR crystal-container:/ros2_ws/src/
-docker exec crystal-container /bin/bash -c 'source /opt/ros/crystal/setup.bash && cd /ros2_ws/ && rosdep install --from-paths src --ignore-src --rosdistro crystal -r -y'
-docker exec crystal-container /bin/bash -c 'source /opt/ros/crystal/setup.bash && cd /ros2_ws/ && colcon build --symlink-install'
-docker exec crystal-container /bin/bash -c 'source /opt/ros/crystal/setup.bash && cd /ros2_ws/ && colcon test'
-docker exec crystal-container /bin/bash -c 'source /opt/ros/crystal/setup.bash && cd /ros2_ws/ && colcon test-result --all'
+ros2_distro_names_arr=($ros2_distro_names)
+
+for (( i=0; i<${#ros2_distro_names_arr[@]}; i++ ));
+do
+  docker pull ros:${ros2_distro_names_arr[$i]}-ros-core
+  docker run -v "$PWD/shared:/shared" -e ROS_DISTRO=${ros2_distro_names_arr[$i]} --name ${ros2_distro_names_arr[$i]}-container -dit ros:${ros2_distro_names_arr[$i]}-ros-core /bin/bash
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'mkdir -p /"$ROS_DISTRO"_ws/src'
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3 python3-pip libgtest-dev cmake && rosdep update'
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib'
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools'
+  docker cp $TRAVIS_BUILD_DIR ${ros2_distro_names_arr[$i]}-container:/"$ROS_DISTRO"_ws/src/
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y'
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage"'
+  if [[ -z "${PACKAGE_NAME}" ]]; then
+    docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --packages-select $PACKAGE_NAME --cmake-target tests
+  fi
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test'
+  docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && source ./install/setup.bash && colcon test-result --all'
+done
+
+IFS=$OIFS;
