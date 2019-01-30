b856cebded96926f52c5fbbbd21db6797189aee9
Abby Xu <abbyxu@amazon.com>
2019-01-29 22:50:20 -0800

    fix build scripts bug

--- ros2_build.sh
+++ ros2_build.sh
@@ -13,7 +13,7 @@ do
   docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3 python3-pip libgtest-dev cmake && rosdep update'
   docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib'
   docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools'
-  docker cp $TRAVIS_BUILD_DIR ${ros2_distro_names_arr[$i]}-container:/"$ROS_DISTRO"_ws/src/
+  docker cp $TRAVIS_BUILD_DIR ${ros2_distro_names_arr[$i]}-container:/${ros2_distro_names_arr[$i]}_ws/src/
   docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y'
   docker exec ${ros2_distro_names_arr[$i]}-container /bin/bash -c 'source /opt/ros/$ROS_DISTRO/setup.bash && cd /"$ROS_DISTRO"_ws/ && colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage"'
   if [[ -z "${PACKAGE_NAME}" ]]; then
