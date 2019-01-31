8d238b47bb553fc7188d9006bfb60087c7027cfe
Abby Xu <abbyxu@amazon.com>
2019-01-31 12:26:52 -0800

    fix bug

--- ros1_build.sh
+++ ros1_build.sh
@@ -3,7 +3,7 @@
 apt update && apt install -y lcov python3-pip libgtest-dev cmake && rosdep update
 cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
 apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
-source /opt/ros/$ROS_DISTRO/setup.bash
+. /opt/ros/$ROS_DISTRO/setup.sh
 cd /"$ROS_DISTRO"_ws/
 rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y
 colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage'
@@ -11,11 +11,11 @@ if [ ! -z "${PACKAGE_NAME}" ];
 then
   colcon build --packages-select $PACKAGE_NAME --cmake-target tests
 fi
-source ./install/setup.bash
+. ./install/setup.sh
 colcon test
 colcon test-result --all
 lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
-lcov --list coverage.info'
+lcov --list coverage.info
 cd /"$ROS_DISTRO"_ws/
 mv coverage.info /shared
