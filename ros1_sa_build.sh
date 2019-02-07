4129e064759ff26bc77ed2c302739c048566ff15
Abby Xu <abbyxu@amazon.com>
2019-02-07 14:37:29 -0800

    add bundle

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-apt update && apt install -y python3-pip dpkg ros-$ROS_DISTRO-ros-base && rosdep update
-apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
+apt update && apt install -y python3-pip python3-apt dpkg ros-$ROS_DISTRO-ros-base && rosdep update
+apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools && pip3 install --upgrade pip
 . /opt/ros/$ROS_DISTRO/setup.sh
 cd /"$ROS_DISTRO"_ws/"$SA_NAME"/robot_ws/
 rosws update
 rosdep install --from-paths src --ignore-src -r -y
-colcon build
+colcon build --build-base build --install-base install
+colcon bundle --build-base build --install-base install --bundle-base bundle
