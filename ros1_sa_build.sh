73a313734889432a0f900957bfe66d0cf1e210d8
Abby Xu <abbyxu@amazon.com>
2019-02-07 15:32:56 -0800

    fix installation bug

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -10,5 +10,13 @@ rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
 colcon bundle --build-base build --install-base install --bundle-base bundle
+mv ./bundle/output.tar.gz ./bundle/robot.tar.gz
+cd /"$ROS_DISTRO"_ws/"$SA_NAME"/simulation_ws/
+rosws update
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --build-base build --install-base install
+colcon bundle --build-base build --install-base install --bundle-base bundle
+mv ./bundle/output.tar.gz ./bundle/simulation.tar.gz
 cp /"$ROS_DISTRO"_ws/"$SA_NAME"/appspec.yml /shared/appspec.yml
-mv /"$ROS_DISTRO"_ws/"$SA_NAME"/robot_ws/bundle/output.tar.gz /shared/output.tar.gz
+mv /"$ROS_DISTRO"_ws/"$SA_NAME"/robot_ws/bundle/robot.tar.gz /shared/robot.tar.gz
+mv /"$ROS_DISTRO"_ws/"$SA_NAME"/simulation_ws/bundle/simulation.tar.gz /shared/simulation.tar.gz
