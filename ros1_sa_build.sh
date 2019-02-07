1082112b4709e617de71e016292cd792e486988e
Abby Xu <abbyxu@amazon.com>
2019-02-07 14:44:20 -0800

    sa artifacts

--- ros1_sa_build.sh
+++ ros1_sa_build.sh
@@ -9,3 +9,5 @@ rosws update
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base build --install-base install
 colcon bundle --build-base build --install-base install --bundle-base bundle
+cp /"$ROS_DISTRO"_ws/"$SA_NAME"/appspec.yml /shared/appspec.yml
+mv /"$ROS_DISTRO"_ws/"$SA_NAME"/robot_ws/bundle/output.tar.gz /shared/output.tar.gz
