b6c9c579dec14add335255ab596d01d52acbd95e
Abby Xu <abbyxu@amazon.com>
2019-02-17 23:47:30 -0800

    pipeline migration

--- ros1_build.sh
+++ ros1_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+# install dependencies
 apt update && apt install -y lcov python3-pip libgtest-dev cmake && rosdep update
 cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
 apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
+
+# use colcon as build tool to build the package, and optionally build tests
 . /opt/ros/$ROS_DISTRO/setup.sh
 cd /"$ROS_DISTRO"_ws/
 rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y
@@ -12,9 +15,13 @@ if [ ! -z "${PACKAGE_NAME}" ];
 then
   colcon build --packages-select $PACKAGE_NAME --cmake-target tests
 fi
+
+# run unit tests
 . ./install/setup.sh
 colcon test
 colcon test-result --all
+
+# get unit test code coverage result
 lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
 lcov --list coverage.info
--- ros2_build.sh
+++ ros2_build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+# install dependencies
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
 echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list
 apt update && apt install -y python3 python3-pip libgtest-dev lcov cmake && rosdep update
 cd /usr/src/gtest && cmake CMakeLists.txt && make && cp *.a /usr/lib
 apt update && apt install -y python3-colcon-common-extensions && pip3 install -U setuptools
+
+# use colcon as build tool to build the package, and optionally build tests
 . /opt/ros/$ROS_DISTRO/setup.sh
 cd /"$ROS_DISTRO"_ws/
 rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -r -y
@@ -14,9 +17,13 @@ if [ ! -z "${PACKAGE_NAME}" ] && [ "$ROS_VERSION" == "1" ];
 then
   colcon build --packages-select $PACKAGE_NAME --cmake-target tests
 fi
+
+# run unit tests
 . ./install/setup.sh
 colcon test
 colcon test-result --all
+
+# get unit test code coverage result
 lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
 lcov --list coverage.info
